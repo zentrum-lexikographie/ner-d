fb68e79c5e92d939c199f76ae65dee3c629a0b1b
Luise Köhler <luise.koehler@bbaw.de>
2025-04-14 14:48:03 +0200

    Process SmartData dataset

--- convert.py
+++ convert.py
@@ -1,6 +1,9 @@
+import gzip
 import re
 import os
 from pathlib import Path
+
+import jsonstream
 from spacy.tokens import DocBin
 from spacy.training.converters import conll_ner_to_docs
 from wasabi import msg
@@ -49,6 +52,66 @@ def iter_germeval(file):
 for bucket, file in germeval_splits.items():
     docs = conll_ner_to_docs(
         iter_germeval(file), n_sents=1, merge_subtokens=True, no_print=True
+    )
+    db = DocBin(docs=docs, store_user_data=True)
+    doc_bins[bucket].merge(db)
+
+msg.divider("Preprocessing SmartData")
+
+smartdata_splits = {
+    "train": assets_dir / "smartdata_train.json.gz",
+    "dev": assets_dir / "smartdata_dev.json.gz",
+    "test": assets_dir / "smartdata_test.json.gz",
+}
+
+SMARTDATA_TO_CONLL = {
+    "DATE": "O",
+    "DISASTER_TYPE": "O",
+    "DISTANCE": "O",
+    "DURATION": "O",
+    "LOCATION": "LOC",
+    "LOCATION_CITY": "LOC",
+    "LOCATION_ROUTE": "O",
+    "LOCATION_STOP": "LOC",
+    "LOCATION_STREET": "LOC",
+    "NUMBER": "O",
+    "ORGANIZATION": "ORG",
+    "ORGANIZATION_COMPANY": "ORG",
+    "ORG_POSITION": "O",
+    "PERSON": "PER",
+    "TIME": "O",
+    "TRIGGER": "O",
+}
+
+
+def iter_smartdata(file, tag_mapping):
+    tokens = []
+    with gzip.open(file) as fh:
+        data = jsonstream.loads(fh.read())
+        for doc in data:
+            text = doc["text"]["string"]
+            for token in doc["tokens"]["array"]:
+                start = token["span"]["start"]
+                end = token["span"]["end"]
+                tok = text[start:end]
+                tags = token["ner"]["string"].split("-", 1)
+                if len(tags) == 2:
+                    iob, ent = tags
+                    ent = tag_mapping[ent]
+                    tag = "-".join([iob, ent]) if ent != "O" else ent
+                else:
+                    tag = tags[0]
+                tokens.append("\t".join([tok, tag]))
+            tokens.append("\n")
+    return "\n".join(tokens)
+
+
+for bucket, file in smartdata_splits.items():
+    docs = conll_ner_to_docs(
+        iter_smartdata(file, SMARTDATA_TO_CONLL),
+        n_sents=1,
+        merge_subtokens=True,
+        no_print=True,
     )
     db = DocBin(docs=docs, store_user_data=True)
     doc_bins[bucket].merge(db)
--- download.sh
+++ download.sh
@@ -6,4 +6,7 @@
 gdown https://drive.google.com/drive/folders/1kC0I2UGl2ltrluI9NqDjaQJGw5iliw_J --folder -O assets
 rm assets/*pdf assets/CHANGELOG assets/NER-de-test-unlabeled.tsv
 
-#
+# SmartData
+wget -c "https://github.com/DFKI-NLP/smartdata-corpus/raw/master/v3_20200302/test.json.gz" -O assets/smartdata_test.json.gz
+wget -c "https://github.com/DFKI-NLP/smartdata-corpus/raw/master/v3_20200302/train.json.gz" -O assets/smartdata_train.json.gz
+wget -c "https://github.com/DFKI-NLP/smartdata-corpus/raw/master/v3_20200302/dev.json.gz" -O assets/smartdata_dev.json.gz
